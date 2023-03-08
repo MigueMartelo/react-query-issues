b27922724256af33baac5a0e35d5e6528ad2382d
Migue Martelo <miguemartelo1031@gmail.com>
2023-03-07 22:10:28 -0500

    feat: show the issues and their comments on their own page

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -2,5 +2,8 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {},
+  headers: {
+    Authorization:
+      'Bearer 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',
+  },
 });
