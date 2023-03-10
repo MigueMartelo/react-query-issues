fea79a7a33016df30eade22fad9092f8fb72deab
Migue Martelo <miguemartelo1031@gmail.com>
2023-03-10 08:46:12 -0500

    feat: add preSetData queries

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {
-    Authorization:
-      'Bearer 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',
-  },
+  headers: {},
 });
