16f441dbd372b90cc5888e3b27bc6ad3385ce022
Migue Martelo <miguemartelo1031@gmail.com>
2023-03-10 20:10:48 -0500

    feat: add filter to state: open and closed

--- src/issues/components/IssueList.tsx
+++ src/issues/components/IssueList.tsx
@@ -1,32 +1,36 @@
 import { FC } from 'react';
-import { Issue } from '../interfaces';
+import { Issue, State } from '../interfaces';
 import { IssueItem } from './IssueItem';
 
 interface Props {
   issues: Issue[];
+  state?: State;
+  onStateChange: (state?: State) => void;
 }
 
-export const IssueList: FC<Props> = ({ issues }) => {
-  return (
-    <div className='card border-white'>
-      <div className='card-header bg-dark'>
-        <ul className='nav nav-pills card-header-pills'>
-          <li className='nav-item'>
-            <a className='nav-link active'>All</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link'>Open</a>
-          </li>
-          <li className='nav-item'>
-            <a className='nav-link'>Closed</a>
-          </li>
-        </ul>
-      </div>
-      <div className='card-body text-dark'>
-        {issues.map((issue) => (
-          <IssueItem key={issue.id} issue={issue} />
-        ))}
-      </div>
+export const IssueList: FC<Props> = ({ issues, state, onStateChange }) => (
+  <div className='card border-white'>
+    <div className='card-header bg-dark'>
+      <ul className='nav nav-pills card-header-pills'>
+        <li className='nav-item' onClick={() => onStateChange()}>
+          <a className={`nav-link ${!state ? 'active' : ''}`}>All</a>
+        </li>
+        <li className='nav-item' onClick={() => onStateChange(State.Open)}>
+          <a className={`nav-link ${state === State.Open ? 'active' : ''}`}>
+            Open
+          </a>
+        </li>
+        <li className='nav-item' onClick={() => onStateChange(State.Closed)}>
+          <a className={`nav-link ${state === State.Closed ? 'active' : ''}`}>
+            Closed
+          </a>
+        </li>
+      </ul>
     </div>
-  );
-};
+    <div className='card-body text-dark'>
+      {issues.map((issue) => (
+        <IssueItem key={issue.id} issue={issue} />
+      ))}
+    </div>
+  </div>
+);
--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,13 +1,26 @@
 import { useQuery } from '@tanstack/react-query';
 import { githubApi } from '../../api/githubApi';
-import { Issue } from '../interfaces';
+import { sleep } from '../../helpers/sleep';
+import { Issue, State } from '../interfaces';
 
-const getIssues = async (): Promise<Issue[]> => {
-  const { data } = await githubApi.get<Issue[]>('/issues');
+interface Props {
+  state?: State;
+  labels: string[];
+}
+
+const getIssues = async (labels: string[], state?: State): Promise<Issue[]> => {
+  sleep(2);
+  const params = new URLSearchParams();
+
+  if (state) params.append('state', state);
+
+  const { data } = await githubApi.get<Issue[]>('/issues', { params });
   return data;
 };
 
-export const useIssues = () => {
-  const issuesQuery = useQuery(['issues'], getIssues);
+export const useIssues = ({ state, labels }: Props) => {
+  const issuesQuery = useQuery(['issues', { state, labels }], () =>
+    getIssues(labels, state)
+  );
   return { issuesQuery };
 };
